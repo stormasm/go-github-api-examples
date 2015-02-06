f6b268f888d1c8071330e1226ffd4a32b980031f
Michael Angerman <stormasm@gmail.com>
2015-02-05 21:56:26 -0500

    using auth client for the first time

--- examples/activity/eventspublic.go
+++ examples/activity/eventspublic.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"code.google.com/p/goauth2/oauth"
 	"fmt"
 	"github.com/google/go-github/github"
+	"os"
 )
 
+var (
+	client *github.Client
+
+	// auth indicates whether tests are being run with an OAuth token.
+	// Tests can use this flag to skip certain tests when run without auth.
+	auth bool
+)
+
+func init() {
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	if token == "" {
+		print("!!! No OAuth token.  Some tests won't run. !!!\n\n")
+		client = github.NewClient(nil)
+	} else {
+		t := &oauth.Transport{
+			Token: &oauth.Token{AccessToken: token},
+		}
+		client = github.NewClient(t.Client())
+		auth = true
+	}
+}
+
 func main() {
-	client := github.NewClient(nil)
 
 	opt := &github.ListOptions{Page: 2}
 	events, _, err := client.Activity.ListEvents(opt)
